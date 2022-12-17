64afcdd50480fb347e6c7093fe2faf618227071d
rizaadi <rizaadi890@gmail.com>
2022-12-17 15:53:47 +0700

    feat: add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 import React from 'react';
@@ -9,6 +10,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute='class' defaultTheme='dark' enableSystem={false}>
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   );
 }
