c8c6f2a8a99348eb8b422055eca2a646d17868de
rizaadi <rizaadi890@gmail.com>
2023-01-16 21:54:49 +0700

    style: add border bottom header

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -2,10 +2,19 @@ import clsx from 'clsx';
 import Link from 'next/link';
 import { useTheme } from 'next-themes';
 import { FiMoon, FiSun } from 'react-icons/fi';
+
+import useScroll from '../../hooks/useScroll';
 function Header() {
   const { theme, setTheme } = useTheme();
+  const scrolled = useScroll(50);
+
   return (
-    <header className='sticky top-0 z-50 transition-shadow backdrop-blur-md'>
+    <header
+      className={clsx(
+        'sticky top-0 z-50 transition-all backdrop-blur-md dark:border-gray-800 dark:bg-black/50 border-gray-200 bg-white/50',
+        scrolled && 'border-b'
+      )}
+    >
       <nav className='grid items-center grid-cols-3 py-5 layout'>
         <div className='justify-self-start'></div>
         <ul className='flex items-center self-center space-x-3 text-base font-medium justify-self-center md:space-x-11 dark:text-black-fifth '>
