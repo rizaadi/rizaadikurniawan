1f759aec11a4b0c50a470ad87208d982a3bd0278
rizaadi <rizaadi890@gmail.com>
2023-04-22 18:21:09 +0700

    style: change color & icon list blogs

--- components/content/blog/BlogContent.tsx
+++ components/content/blog/BlogContent.tsx
@@ -10,17 +10,14 @@ type BlogContentProps = {
 function BlogContent({ post }: BlogContentProps) {
   return (
     <Link href={`/blog/${post.slug}`} passHref>
-      <li>
-        <div className='pb-9'>
-          <p className='text-gray-700 dark:text-white'>
-            {dayjs(post.publishedAt).format('MMMM D YYYY')} &mdash;{' '}
-            {post.readingTime.text} &mdash;{' '}
-            {post.views?.toLocaleString() ?? '–––'}
-          </p>
-          <h1 className='mt-2'>{post.title}</h1>
-          <p className='mt-3 '>{post.description}</p>
-          <p className='mt-4'>READ MORE</p>
-        </div>
+      <li className='pb-9'>
+        <p>
+          {dayjs(post.publishedAt).format('MMMM D YYYY')} &#183;{' '}
+          {post.readingTime.text} &#183; {post.views?.toLocaleString() ?? '–––'}{' '}
+          views
+        </p>{' '}
+        <h1 className='mt-2'>{post.title}</h1>
+        <p className='mt-3'>{post.description}</p>
       </li>
     </Link>
   );
