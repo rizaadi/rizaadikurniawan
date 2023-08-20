7be8893aa9d3e5b6cebe60d4218664900cdd2709
rizaadi <rizaadi890@gmail.com>
2023-08-20 12:09:14 +0700

    style: animating hover table of contents

--- components/content/TableOfContents.tsx
+++ components/content/TableOfContents.tsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx';
+import { m } from 'framer-motion';
 import Link from 'next/link';
 
 export type HeadingScrollSpy = Array<{
@@ -15,27 +16,32 @@ type TableOfContentsProps = {
 export default function TableOfContents({
   toc,
   activeSection,
+  minLevel,
 }: TableOfContentsProps) {
   return (
     <div id='toc-container' className='hidden overflow-auto lg:block'>
-      <h4 className='mb-3'>Table Of Contents</h4>
+      <h4 className='mb-2'>Table Of Contents</h4>
       <div className='flex flex-col space-y-1 text-sm max-h-[calc(100vh-15rem)]'>
         {toc
-          ? toc.map(({ id, text }) => (
-              <Link
-                replace
-                scroll={false}
+          ? toc.map(({ id, text, level }) => (
+              <m.div
+                whileHover={{
+                  x: 2.5,
+                  transition: { type: 'spring', stiffness: 400, damping: 17 },
+                }}
                 key={id}
-                href={`#${id}`}
                 className={clsx(
-                  'dark:hover:text-white-primary hover:text-black-primary font-medium',
+                  'dark:hover:text-white-primary hover:text-black-primary font-medium w-fit',
                   activeSection === id
                     ? 'text-black-primary dark:text-white-primary'
                     : 'text-black-secondary dark:text-gray-500'
                 )}
+                style={{ marginLeft: (level - minLevel) * 15 }}
               >
-                {text}
-              </Link>
+                <Link key={id} href={`#${id}`} replace scroll={false}>
+                  {text}
+                </Link>
+              </m.div>
             ))
           : null}
       </div>
