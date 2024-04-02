286086e720e9aeae3f7b6b21828458bda328030d
rizaadi <rizaadi890@gmail.com>
2024-04-02 22:09:38 +0700

    fix: open graph seo

--- components/Seo.tsx
+++ components/Seo.tsx
@@ -39,7 +39,7 @@ export default function Seo(props: SeoProps) {
       <meta property='og:site_name' content={meta.siteName} />
       <meta property='og:description' content={meta.description} />
       <meta property='og:title' content={meta.title} />
-      <meta name='image' property='og:image' content={meta.image} />
+      <meta property='og:image' content={meta.image} />
       <meta name='twitter:card' content='summary_large_image' />
       <meta name='twitter:title' content={meta.title} />
       <meta name='twitter:description' content={meta.description} />
